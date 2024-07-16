9ee258ffcedc21c48d498e5e471b51d1a040e7e4
Isaac Toyin <isaacp.official@gmail.com>
2024-07-16 10:19:03 +0100

    Added Spatie package

--- backendtest/database/migrations/0001_01_01_000000_create_users_table.php
+++ backendtest/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Enums\UserStatus;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status',UserStatus::values())->default(UserStatus::ACTIVE->value);
             $table->rememberToken();
             $table->timestamps();
         });
